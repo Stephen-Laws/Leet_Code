137d955263f6d06b3d2058e8d66a25078a1af309
Stephen-Laws <sl6114@ic.ac.uk>
2022-06-06 13:04:05 +0100

    13 shorter sol, less readable

--- python/0-99/013_roman_to_integer.py
+++ python/0-99/013_roman_to_integer.py
@@ -6,21 +6,11 @@ class Solution(object):
         """
         # I = 1, V = 5, X = 10, L = 50, C = 100, D = 500, M = 1000
         keys = {"I" : 1, "V" : 5, "X" : 10, "L" : 50, "C" : 100, "D": 500, "M": 1000}
-        special_vals = [1,10,100]
-        val = 0
-        len_s = len(s)
-        for i, char in enumerate(s):
-            curr_val = keys[char]
-            if curr_val in special_vals:
-                if i < len_s-1:
-                    next_val = keys[s[i+1]]
-                    if curr_val < next_val:
-                        val -= curr_val
-                    else:
-                        val += curr_val
-                else:
-                    val += curr_val
+        val = 0 
+        for i in range(len(s) - 1):
+            if keys[s[i]] < keys[s[i+1]]:
+                val -= keys[s[i]]
             else:
-                val += curr_val
-            
-        return val
+                val += keys[s[i]]
+           
+        return val + keys[s[i-1]]
