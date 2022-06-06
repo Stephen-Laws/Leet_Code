63d99b156eb5a715f4c5672af09d7794760cf564
Stephen-Laws <sl6114@ic.ac.uk>
2022-06-06 14:31:45 +0100

    one pass two sum

---
+++ python/0-99/001_two_sum.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        results_dict = {}
+        for i, num in enumerate(nums):
+            res = target - num
+            if res in results_dict:
+                return [i, results_dict[res]]
+            else:
+                results_dict[num] = i
+
--- python/0-99/README.md
+++ python/0-99/README.md
@@ -2,7 +2,8 @@ Leetcode Problems 0-99 (PYTHON)
 
 |NO.|Title|Solution|Add Date|Difficulty|
 |---|-----|--------|--------|----------|
+|1| [Two Sum][1]|001_two_sum.py|22/06/06|Easy|
 |13|[Roman to Integer][13]|013_roman_to_integer.py|22/06/06|Easy|
 
-
+[1]:https://oj.leetcode.com/problems/two-sum/
 [13]:https://oj.leetcode.com/problems/roman-to-integer/
