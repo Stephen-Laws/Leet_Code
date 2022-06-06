e4c58ee204a3df995d9a9a8458fec72b56920d85
Stephen-Laws <sl6114@ic.ac.uk>
2022-06-06 16:28:15 +0100

    tryting to carry linked list

--- python/0-99/002_add_two_numbers.py
+++ python/0-99/002_add_two_numbers.py
@@ -10,23 +10,43 @@ class Solution(object):
         :type l2: ListNode
         :rtype: ListNode
         """
-        l1_val = self.getVal(l1)
-        l2_val = self.getVal(l2)
-        res = l1_val+l2_val
         head = node = ListNode(0)
-        for digit in reversed(str(res)):
-            d = int(digit)
-            node.next = ListNode(d)
-            node = node.next
-            print(head.next)
         
-        return head.next
+        carry = 0
+        
+        while l1 or l2 or carry:
+            if l1:
+                carry += l1.val
+                l1 = l1.next
+            if l2:
+                carry += l2.val
+                l2 = l2.next
+            carry, val = divmod(carry, 10)
+            node.next = ListNode(val) 
+            node = node.next
             
-    def getVal(self, l):
-        val = l.val
-        exp = 10
-        while l.next != None:
-            l = l.next
-            val += l.val*exp
-            exp*= 10
-        return val
+        return head.next
+
+
+
+
+    #     l1_val = self.getVal(l1)
+    #     l2_val = self.getVal(l2)
+    #     res = l1_val+l2_val
+    #     head = node = ListNode(0)
+    #     for digit in reversed(str(res)):
+    #         d = int(digit)
+    #         node.next = ListNode(d)
+    #         node = node.next
+    #         print(head.next)
+        
+    #     return head.next
+            
+    # def getVal(self, l):
+    #     val = l.val
+    #     exp = 10
+    #     while l.next != None:
+    #         l = l.next
+    #         val += l.val*exp
+    #         exp*= 10
+    #     return val
