363ab5f2df90eb966554f87f1a30b874b78ca588
Stephen Laws <sl6114@ic.ac.uk>
2022-06-13 11:08:54 +0100

    binary_search

---
+++ python/0-99/704_binary_search.py
@@ -0,0 +1,18 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        upper_idx = len(nums)
+        lower_idx = 0
+        idx = (upper_idx-lower_idx)//2
+        prev_idx = -1
+        while (1):
+            if idx == prev_idx:
+                return -1
+            elif target > nums[idx]:
+                lower_idx = idx
+            elif target < nums[idx]:
+                upper_idx = idx
+            else:
+                return idx
+            prev_idx = idx
+            idx = (upper_idx-lower_idx)//2 + lower_idx
+                    
