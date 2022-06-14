a6358d30b268c52a25f2f4c28112c49c181a30c0
Stephen Laws <sl6114@ic.ac.uk>
2022-06-14 09:41:34 +0100

    2 binary search problems

---
+++ python/800-899/852_peak_index.py
@@ -0,0 +1,12 @@
+class Solution:
+    def peakIndexInMountainArray(self, arr: List[int]) -> int:
+        l, r = 0, len(arr)-1
+        idx = (r-l)//2
+        i = 0
+        while r > l+1:
+            if arr[idx] > arr[idx+1]:
+                r = idx
+            else:
+                l = idx
+            idx = (r-l)//2+l
+        return r 
