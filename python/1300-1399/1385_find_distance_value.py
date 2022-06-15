83a92c92a98f0d219491a8e00186fe8325941ca7
Stephen-Laws <sl6114@ic.ac.uk>
2022-06-15 11:07:21 +0100

    amended names

---
+++ python/1300-1399/1385_find_distance_value.py
@@ -0,0 +1,14 @@
+class Solution:
+    def findTheDistanceValue(self, arr1: List[int], arr2: List[int], d: int) -> int:
+        
+
+
+
+        #Brute force method
+        # distance = len(arr1)
+        # for i in arr1:
+        #     for j in arr2:
+        #         if abs(i-j) <= d:
+        #             distance -= 1 
+        #             break
+        # return distance
