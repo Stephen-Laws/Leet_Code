7da97a6357a74d3729c338694e242c56a0840f67
Stephen Laws <sl6114@ic.ac.uk>
2022-06-16 16:52:20 +0100

    inefficient 3 solution

---
+++ python/0-99/0003_longest_substring.py
@@ -0,0 +1,21 @@
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        if s == "":
+            return 0
+        length = 1
+        s_len = len(s)
+        unique_str = [s[0]]
+        for i in range(1,s_len):
+            if s[i] in unique_str:
+                repeat_idx = unique_str.index(s[i])
+                j = -1
+                while j < repeat_idx:
+                    unique_str.pop(0)
+                    j+=1
+            unique_str.append(s[i])
+            
+            if len(unique_str) > length:
+                length = len(unique_str)
+            
+        return length                 
+        
