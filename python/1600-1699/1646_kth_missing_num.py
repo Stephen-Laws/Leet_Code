c3d40097abee41b20f11db5bd4d8ab2980ce7e03
Stephen-Laws <sl6114@ic.ac.uk>
2022-06-20 09:11:27 +0100

    problem 1646

--- python/1600-1699/1646_kth_missing_num.py
+++ python/1600-1699/1646_kth_missing_num.py
@@ -2,4 +2,26 @@
 
 class Solution:
     def findKthPositive(self, arr: List[int], k: int) -> int:
+        arr_len = len(arr)
+        max_arr = arr[-1]
+        missing_ints = max_arr - arr_len
         
+        if k > missing_ints:
+            return max_arr + k-missing_ints
+        
+        if arr[0] > k:
+            return k
+        
+        #else binary search
+        lo = 0
+        hi = arr_len
+        idx = (arr_len + lo) // 2
+        
+        while hi > lo:
+            if ((arr[idx] -idx) <= k):
+                lo = idx +1
+            else:
+                hi = idx
+            idx = (hi+lo)//2
+
+        return idx +k
